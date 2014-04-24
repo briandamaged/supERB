497aaed1ae60b183d322ea7ed866fd486a09e7d1
Brian Lauber <constructible.truth@gmail.com>
2014-04-23 21:16:54 -0400

    Added method_missing stuffs for easier assignment

--- lib/superb.rb
+++ lib/superb.rb
@@ -23,6 +23,23 @@ module Superb
       self.instance_variable_set(field_name_for(key), value)
     end
 
+    def method_missing(name, *args, &block)
+      name_str = name.to_s
+
+      if name_str.end_with? "="
+
+        self.[]=(name[0...-1].to_sym, *args, &block)
+      else
+        self.[](name, *args, &block)
+      end
+    end
+
+    def respond_to?(name)
+      # TODO: Should only return true when string can be
+      #       used as a method name.
+      true
+    end
+
     def merge!(hashable)
       hashable.to_hash.each do |k, v|
         self[k] = v
